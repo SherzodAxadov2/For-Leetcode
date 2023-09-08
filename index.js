bb57e3acbda1cf2e9a29b5bdac4d76e106718984
Axadov Sherzod <sh.axadov@uic.group>
2023-09-08 19:24:32 +0500

    commit

--- index.js
+++ index.js
@@ -262,4 +262,4 @@ var removeDuplicates = function (nums) {
   return index;
 };
 
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
